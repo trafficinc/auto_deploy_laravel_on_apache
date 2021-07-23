77e0947d8f100ece5a0adaafd7139581480d7218
Ron B <trafficinc@users.noreply.github.com>
2021-07-23 16:37:54 -0400

    Create inst_php_deploy.sh
    init

---
+++ inst_php_deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir releases
+mkdir shared
+mkdir .dep
+mkdir .dep/repo
+
+touch .dep/releases.json
+touch .dep/releases.txt
+
+echo "Manully put .env and storage folder from project into shared folder for now, todo: write into this script."
